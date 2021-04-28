6d0571f1caf044043fa226116f977f4961107bc3
annawojdz <83343546+annawojdz@users.noreply.github.com>
2021-04-28 12:14:40 +0200

    Add files via upload

---
+++ 10 zajęcia.py
@@ -0,0 +1,10 @@
+print("podaj pierwszą liczbę")
+liczba1 = int(input())
+print("podaj drugą liczbę")
+liczba2 = int(input())
+if liczba1>liczba2:
+    print(liczba1)
+elif liczba1<liczba2:
+    print(liczba2)
+else:
+    print("Liczby są równe")
